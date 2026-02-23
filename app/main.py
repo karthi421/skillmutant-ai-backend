335678a5d35066cb20bd996a02984f65fa705789
karthik <kandellikarthik7@gmail.com>
2026-02-23 11:28:41 +0530

    rooms updated

--- app/main.py
+++ app/main.py
@@ -458,7 +458,6 @@ app.include_router(router)
 
 @app.websocket("/ws/rooms/{room_id}/{user_id}")
 async def room_ws(websocket: WebSocket, room_id: str, user_id: str):
-    await websocket.accept()
     await room_signaling.connect(room_id, user_id, websocket)
 
     try:
